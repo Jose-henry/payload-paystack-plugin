cff5aa8590aabf47f8c471876dc9992a63c97780
Jose-henry <josephhemry093@gmail.com>
2025-06-02 11:05:27 +0100

    fixed HMR and Importmap

--- src/client.ts
+++
@@ -1,5 +0,0 @@
-// This file exists to satisfy Payload's import map generator
-// The plugin doesn't have any client-side components yet
-
-// Export client-side components
-export { LinkToDoc } from './ui/LinkToDoc.jsx'
